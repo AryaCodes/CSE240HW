a42e3dc4a611d4952f3534d2577cad00e0e1e57f
Arya <aryamt2003@gmail.com>
2020-05-26 08:21:09 -0700

    New

--- arya_mathloop.c
+++ arya_mathloop.c
@@ -29,7 +29,7 @@ switch (ch)
 case '-': f = (double)a - b; printf("f = %d\n", f); break; 
 case '*': f = (double)a * b; printf("f = %d\n", f); break; 
 case '/': f = ((double)a / b); printf("f = %f\n", f); break;
-case '%': f = (double)a%b;  printf("f = %d\n", f); break;
+case '%': f = (double)a%(double)b;  printf("f = %d\n", f); break;
 case 'q': break;
 default: printf("invalid operator\n");  } 
 
