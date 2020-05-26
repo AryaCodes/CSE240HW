f5d4836c13e290506a658c8184a9abed10f63d9d
Arya <aryamt2003@gmail.com>
2020-05-26 07:17:23 -0700

    Corrected

--- arya_mathloop.c
+++ arya_mathloop.c
@@ -1,19 +1,4 @@
-#include <stdio.h>
-this is changed
-void main()
-{
-char ch;
-ch = '+';
-int f, a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f);
-case '-': f = a - b; printf("f = %d\n", f);
-case '*': f = a * b; printf("f = %d\n", f);
-case '/': f = a / b; printf("f = %d\n", f);
-default: printf("invalid operator\n");
-}
-ch = '-';
+
 #include <stdio.h>
 void main()
 {
