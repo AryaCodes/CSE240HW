494fb4b831cad6a8255eca91f02e68846b2ab217
Arya <aryamt2003@gmail.com>
2020-05-26 07:48:49 -0700

    New

--- arya_mathloop.c
+++ arya_mathloop.c
@@ -1,56 +1,34 @@
 
+*/	CSE 40 Assignment 1: Math Loop
+	Name: Arya Manish Tawde
+	Description: This program loops through user-inputted airthmatic operaters
+		     and performs them on pre-set integers, computes and displays the answer.
+*/
+	
+
 #include <stdio.h>
 void main()
 {
+//Declaring and assigning variables;
 char ch;
-ch = '+';
-int f, a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f); break;
-case '-': f = a - b; printf("f = %d\n", f); break;
-case '*': f = a * b; printf("f = %d\n", f); break;
+int f, a= 10, b=20;
+
+while(1)
+{
+//Asking for input
+printf("Enter the command you want to do!\n+ adds\n- subtracts\n* multiplies\n/ divides\n%% gives remainder\n ");
+
+//Scanning the input
+scanf("%c", &ch);
+
+//Operating the switch appropriately
+switch (ch) 
+{case '+': f = a + b; printf("f = %d\n", f); break;
+case '-': f = a - b; printf("f = %d\n", f); break; 
+case '*': f = a * b; printf("f = %d\n", f); break; 
 case '/': f = a / b; printf("f = %d\n", f); break;
-default: printf("invalid operator\n");
+default: printf("invalid operator\n");  } 
 }
-ch = '-';
-a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f); break;
-case '-': f = a - b; printf("f = %d\n", f); break;
-case '*': f = a * b; printf("f = %d\n", f); break;
-case '/': f = a / b; printf("f = %d\n", f); break;
-default: printf("invalid operator\n");
-}
-ch = '*';
-a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f); break;
-case '-': f = a - b; printf("f = %d\n", f); break;
-case '*': f = a * b; printf("f = %d\n", f); break;
-case '/': f = a / b; printf("f = %d\n", f); break;
-default: printf("invalid operator\n");
-}
-ch = '/';
-a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f); break;
-case '-': f = a - b; printf("f = %d\n", f); break;
-case '*': f = a * b; printf("f = %d\n", f); break;
-case '/': f = a / b; printf("f = %d\n", f); break;
-default: printf("invalid operator\n");
-}
-ch = '%';
-a = 10, b = 20;
-printf("ch = %c\n", ch);
-switch (ch) {
-case '+': f = a + b; printf("f = %d\n", f); break;
-case '-': f = a - b; printf("f = %d\n", f); break;
-case '*': f = a * b; printf("f = %d\n", f); break;
-case '/': f = a / b; printf("f = %d\n", f); break;
-default: printf("invalid operator\n");
-}
+
+
 }
