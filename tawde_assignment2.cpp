3602eb1468a14d1c6d9aacec38b139fb878a4bdd
Arya <aryamt2003@gmail.com>
2020-06-05 05:04:42 -0700

    Assignment 2 Skeleton

--- tawde_assignment2.cpp
+++ tawde_assignment2.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
 #include<cstdlib>
 #include<ctime>
+#inlcude<fstream>
+#include<iostream>
 
 using namespace std;
 
@@ -8,10 +9,15 @@ int menu();
 void miniGames();
 void macrosVFunction();
 int randomInRange( int, int);
+int isInt(char a[]);
+int charToInt(char b);
+int strToInt(char a[], int length);
+int powerCal(int base, int power);
 
-int main()
+int main(int argc, int arg)
 	{
-	srand((unsigned)time(0));
+	
+	srand((unsigned)time(0)); 
 	while(1)
 		{
 		int choice = menu();
@@ -76,11 +82,68 @@ int menu()
 	}
 
 void miniGames()
-{cout<< "In MiniGames rolling d100:"<< randomInRange(1, 100)<< " " << randomInRange(1, 100)  << endl;}
+	{
+	
+	}
+
+
+
+
+
 void macrosVFunction()
-{cout<<"In Macros rolling d6:" << randomInRange(1,6)<< endl;}
+{cout<<"In Macros rolling d6:" << endl;}
 
 int randomInRange(int min, int max)
 	{
 	return (min + (rand()%(max + 1 -min)));
+	}
+
+int isInt(char a[])
+	{
+	int x = 0;
+	while(a[x]!='\0')
+		{
+		if( (a[x]<48) || (a[x]>57) )
+			return 0;	
+		x++;
+		}
+	return x;	
+	}
+
+int strToInt(char a[], int length)
+	{
+	int x = 0;
+	int value = 0;
+	while(x <= length)
+		{
+		value += charToInt(a[x]) * powerCal(10, length -x) 
+		}		
+	return value;
+	}
+
+int charToInt(char b)
+	{
+	switch(b)
+		{
+		case '0': return 0;
+		case '1': return 1;
+		case '2': return 2;
+		case '3': return 3;
+		case '4': return 4;
+		case '5': return 5;
+		case '6': return 6;
+		case '7': return 7;
+		case '8': return 8;
+		case '9': return 9;
+		default: return 0;
+		}
+
+	}
+
+int powerCal(int base, int power)
+	{
+	if(power == 0)
+		return 1;
+	else
+		return (base * powerCal(base, (power-1))); 
 	}
